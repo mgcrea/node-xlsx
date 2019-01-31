7881a9478a576170d6ab9be13273b6b69ee93df1
Olivier Louvignes <olivier@mgcrea.io>
2019-01-31 10:31:53 +0100

    chore(options): allow local option overriding

--- src/index.js
+++ src/index.js
@@ -20,17 +20,11 @@ export function build(worksheets, options = {}) {
   };
   const workBook = new Workbook();
   worksheets.forEach((worksheet) => {
-    const name = worksheet.name || 'Sheet';
-    // If worksheet has options, use this one ; if there's only one worksheet, use global options (for retro-compatibility) 
-    let optionsLocal = {};
-    if (worksheet.options) {
-      optionsLocal = worksheet.options;
-    } else if (worksheets.length === 1 && options) {
-      optionsLocal = options;
-    }
-    const data = buildSheetFromMatrix(worksheet.data || [], optionsLocal);
-    workBook.SheetNames.push(name);
-    workBook.Sheets[name] = data;
+    const sheetName = worksheet.name || 'Sheet';
+    const sheetOptions = worksheet.options || {};
+    const sheetData = buildSheetFromMatrix(worksheet.data || [], {...options, ...sheetOptions});
+    workBook.SheetNames.push(sheetName);
+    workBook.Sheets[sheetName] = sheetData;
   });
   const excelData = XLSX.write(workBook, Object.assign({}, defaults, options));
   return excelData instanceof Buffer ? excelData : bufferFrom(excelData, 'binary');
